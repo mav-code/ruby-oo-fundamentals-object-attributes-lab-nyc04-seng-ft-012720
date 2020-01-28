99ae43e70aee79c4a32613ec3773269ee870b0a3
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-28 16:49:52 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name
+    @name
+  end
+  def name=(personssname)
+    @name = personssname
+  end
+  def job
+    @job
+  end
+  def job=(personsjob)
+    @job = personsjob
+  end
+end
